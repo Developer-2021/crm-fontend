438e873af0f4aeabb7a32e1dfea6928522b1d701
Developer-2012 <raymandchan1@gmail.com>
2021-07-19 09:29:35 +0800

    Dashoard.page.js and dummy tickets.json

---
+++ src/components/breadcrumb/Breadcrumb.comp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Breadcrumb({ page }) {
+  return (
+    <Breadcrumb>
+      <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+      <Breadcrumb.Item active>{page}</Breadcrumb.Item>
+    </Breadcrumb>
+  );
+}
--- src/pages/dashoard/Dashoard.page.js
+++ src/pages/dashoard/Dashoard.page.js
@@ -1,12 +1,43 @@
 import { React } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import { TicketTable } from "../../components/ticket-table/TicketTable.comp";
+import  tickets from "../../assets/data/dummy-tickets.json";
+
+import { Breadcrumb } from "../../components/breadcrumb/Breadcrumb.comp";
+
+
 
 export const Dashboard = () => {
   return (
     <Container>
+        <Row>
+            <Col>
+            </Col>
+        </Row>
       <Row>
-        <Col>
-          <Button>Add New Ticket</Button>
+        <Col className="text-center mt-5 mb-2 ">
+          <Button
+            variant="info"
+            style={{ "font-size": "2rm", padding: "10px 30px" }}
+          >
+            Add New Ticket
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center mb-2">
+        <div>Total ticket: 50</div>
+        <div>Pending ticket: 5</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mt-2">
+        Recently Added tickeds
+        </Col>
+      </Row>
+      <Row>
+        <Col className="recent-ticket">
+            <TicketTable>{tickets}</TicketTable> 
         </Col>
       </Row>
     </Container>
